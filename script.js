0b820e129a9f7252779294c4edd85234751c019c
Rhonda Faulkner <rhondaful@gmail.com>
2015-02-13 20:25:06 -0600

    linked rhondaful website

---
+++ script.js
@@ -0,0 +1,10 @@
+var document.getElementById("bio");
+var moreBio = document.getElementById("more-bio");
+var hiddenText = document.getElementById("more-bio-text");
+
+
+
+
+console.log('Rhonda is rocking this JavaScript!');
+console.warn('A dire warning!');
+console.error('ERMAGERD AN ERROR!');
